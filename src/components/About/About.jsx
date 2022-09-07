5cdf313cad0b4b507b0fccfbb5f975ae5c040a5d
pouyan Kheradmand <pooyan.kheradmand@gmail.com>
2022-09-07 17:52:02 +0200

    about section  added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,21 @@
 import "./About.scss";
 
 export default function About() {
-  return <div className="about" id="about"></div>;
+  return (
+    <div className="about" id="about">
+      <div className="container">
+        <div className="text">
+          <h4>
+            About me: <menu type="toolbar"></menu>
+          </h4>
+          <p>
+            My name is Pouyan and I live in Vienna. I studied industrial
+            engineering in Iran. recently graduated from the CodeFactory
+            Institute in Vienna as a Full Stack Web developer.
+          </p>
+        </div>
+        <img src="assets/imgs/pouyan.jpeg" alt="" />
+      </div>
+    </div>
+  );
 }
